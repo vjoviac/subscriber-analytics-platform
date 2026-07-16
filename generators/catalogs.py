a553e369446c7b1b664969cbc38fc65a5a62151d
vjoviac <joviac@gmail.com>
2026-07-16 08:31:40 -0600

    Creation of catalogs and event model

---
+++ generators/catalogs.py
@@ -0,0 +1,30 @@
+PLANS = [
+    "Unlimited",
+    "Premium",
+    "Standard",
+    "Prepaid"
+]
+
+APPLICATIONS = [
+    "YouTube",
+    "Netflix",
+    "TikTok",
+    "WhatsApp",
+    "Instagram",
+    "Spotify",
+    "Zoom"
+]
+
+RAT_TYPES = [
+    "4G",
+    "5G",
+    "WiFi"
+]
+
+CITIES = [
+    "Ciudad de México",
+    "Guadalajara",
+    "Monterrey",
+    "Puebla",
+    "Querétaro"
+]
